230c2c4ebc5c66095c4bd9b9b54a0c119706ff71
JPool1985 <jakepool.bartender@gmail.com>
2016-02-23 13:59:16 -0500

    Working update rya-throwing

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	})
 	.mousedown(function() {
 		playHadouken();
-		$('.ryu-ready, .ryu-still').hide();
+		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show()
 		.animate(
@@ -31,15 +31,6 @@ $(document).ready(function() {
 
     // play hadouken sound
     // show hadouken and animate it to the right of the screen
-
-    $('.hadouken').show().animate(
-    	{'left': '1020px'},
-    	500,
-    	function() {
-    		$(this).hide();
-    		$(this).css('left', '520px');
-    	}
-    	);
     function playHadouken () {
     	$('#hadouken-sound')[0].volume = 0.5;
     	$('#hadouken-sound')[0].load();
