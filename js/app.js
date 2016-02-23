ac0d7c347dddf24031ac358190bf002d49c9a1bf
JPool1985 <jakepool.bartender@gmail.com>
2016-02-23 04:13:35 -0500

    Reposition Ryu

--- js/app.js
+++ js/app.js
@@ -1,46 +1,45 @@
 
 $(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-  .mousedown(function() {
-  playHadouken();
-  $('.ryu-ready').hide();
-  $('.ryu-throwing').show();
-  $('.hadouken').finish().show()
-  .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $(this).hide();
-      $(this).css('left', '520px');
-    }
-  );
-})
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+			);
+	})
   // animate hadouken to the right of the screen
 })
     // play hadouken sound
     // show hadouken and animate it to the right of the screen
-  })
-.mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
-});
-$('.hadouken').show().animate(
-  {'left': '1020px'},
-  500,
-  function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-  }
-);
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+    .mouseup(function() {
+    	$('.ryu-throwing').hide();
+    	$('.ryu-ready').show();
+    });
+    $('.hadouken').show().animate(
+    	{'left': '1020px'},
+    	500,
+    	function() {
+    		$(this).hide();
+    		$(this).css('left', '520px');
+    	}
+    	);
+    function playHadouken () {
+    	$('#hadouken-sound')[0].volume = 0.5;
+    	$('#hadouken-sound')[0].load();
+    	$('#hadouken-sound')[0].play();
+    }
